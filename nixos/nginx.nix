b819ad292af833dc97a8248536bfb8d2b4888b0e
Kyren223 <Kyren223@proton.me>
2024-12-26 19:43:32 +0200

    Changed localhost to ip

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -3,7 +3,7 @@
     getty.autologinUser = "root";
     nginx = {
       enable = true;
-      virtualHosts.localhost.locations."/" = {
+      virtualHosts."185.170.113.195".locations."/" = {
         index = "index.html";
         root = pkgs.writeTextDir "index.html" ''
           <html>
