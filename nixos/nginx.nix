b25eb441f5a05e9dcd887f6cda46e750e2d62e17
Kyren223 <Kyren223@proton.me>
2024-12-26 22:18:06 +0200

    Added sops for secrets

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -1,23 +1,20 @@
 { pkgs, ... }: {
-  services = {
-    getty.autologinUser = "root";
-    nginx = {
-      enable = true;
-      virtualHosts."185.170.113.195" = {
-        listen = [{
-          addr = "0.0.0.0";
-          port = 80;
-        }];
-        locations."/" = {
-          index = "index.html";
-          root = pkgs.writeTextDir "index.html" ''
-            <html>
-            <body>
-            Hello, world!
-            </body>
-            </html>
-          '';
-        };
+  services.nginx = {
+    enable = true;
+    virtualHosts."185.170.113.195" = {
+      listen = [{
+        addr = "0.0.0.0";
+        port = 80;
+      }];
+      locations."/" = {
+        index = "index.html";
+        root = pkgs.writeTextDir "index.html" ''
+          <html>
+          <body>
+          Hello, world!
+          </body>
+          </html>
+        '';
       };
     };
   };
