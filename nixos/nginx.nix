229bd7677bee3de05515de997f203d0d13fbfd96
Kyren223 <Kyren223@proton.me>
2024-12-26 19:45:15 +0200

    Added port

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -3,15 +3,18 @@
     getty.autologinUser = "root";
     nginx = {
       enable = true;
-      virtualHosts."185.170.113.195".locations."/" = {
-        index = "index.html";
-        root = pkgs.writeTextDir "index.html" ''
-          <html>
-          <body>
-          Hello, world!
-          </body>
-          </html>
-        '';
+      virtualHosts."185.170.113.195" = {
+        listen = [ 80 ];
+        locations."/" = {
+          index = "index.html";
+          root = pkgs.writeTextDir "index.html" ''
+            <html>
+            <body>
+            Hello, world!
+            </body>
+            </html>
+          '';
+        };
       };
     };
   };
