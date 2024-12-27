a148d0dc395b8f5e59f92f1c4199797df7580180
Kyren223 <Kyren223@proton.me>
2024-12-27 15:26:09 +0200

    Trying normal user

--- configuration.nix
+++ configuration.nix
@@ -26,10 +26,10 @@
   ];
 
   users.users.website = {
-    home = "/home/website";
-    createHome = true;
-    isSystemUser = true;
-    group = "users";
+    # home = "/home/website";
+    # createHome = true;
+    isNormalUser = true;
+    # group = "users";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7P9K9D5RkBk+JCRRS6AtHuTAc6cRpXfRfRMg/Kyren"
     ];
