764093669ca404a34e9d8a1a2c72a483c932e473
Kyren223 <Kyren223@proton.me>
2024-12-27 16:37:49 +0200

    Changed to domain

--- nginx.nix
+++ nginx.nix
@@ -1,22 +1,24 @@
 { pkgs, ... }: {
   services.nginx = {
     enable = true;
-    virtualHosts."185.170.113.195" = {
-      extraConfig = ''
-        error_page 404 /404.html;
-        location = /404.html {
-                root /srv/website;
-                internal;
-        }
-      '';
+    virtualHosts."kyren.codes" = {
       listen = [{
         addr = "0.0.0.0";
         port = 80;
       }];
+
       locations."/" = {
         index = "index.html";
         root = "/srv/website";
       };
+
+      locations."/404.html" = {
+        root = "/srv/website";
+      };
+      extraConfig = ''
+        error_page 404 /404.html;
+      '';
+
     };
   };
 
