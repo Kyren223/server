fad957762ed9ffc70f6afbb915c9fd878a2e631d
Kyren223 <Kyren223@proton.me>
2024-12-27 16:04:06 +0200

    Trying to add custom error code in nginx

--- nginx.nix
+++ nginx.nix
@@ -2,6 +2,13 @@
   services.nginx = {
     enable = true;
     virtualHosts."185.170.113.195" = {
+      extraConfig = ''
+        error_page 404 /404.html;
+        location = /404.html {
+                root /srv/website;
+                internal;
+        }
+      '';
       listen = [{
         addr = "0.0.0.0";
         port = 80;
