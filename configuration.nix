1eb7c5f1f4365c77623bc2907390a061c3e15e59
Kyren223 <Kyren223@proton.me>
2024-12-27 15:20:42 +0200

    Changed delay to 5s

--- configuration.nix
+++ configuration.nix
@@ -46,7 +46,7 @@
   system.autoUpgrade = {
     enable = true;
     flake = "github:kyren223/server#default";
-    dates = "minutely"; # Poll interval
+    dates = "5s"; # Poll interval
     flags = [
       "--no-write-lock-file" # Prevent flake.lock from upgrading
       "--option" "tarball-ttl" "0" # Required for polling below 1h
