a203a8f9f89ed24869bb60dc1a1cbff0b049fc7d
Kyren223 <Kyren223@proton.me>
2024-12-27 15:18:34 +0200

    Changed back to every minute

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,7 @@
   system.autoUpgrade = {
     enable = true;
     flake = "github:kyren223/server#default";
-    dates = "1s"; # Poll interval
+    dates = "minutely"; # Poll interval
     flags = [
       "--no-write-lock-file" # Prevent flake.lock from upgrading
       "--option" "tarball-ttl" "0" # Required for polling below 1h
