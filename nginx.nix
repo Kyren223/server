3d14ed35a56716c70638f471aa2f174e905a3f64
Kyren223 <Kyren223@proton.me>
2024-12-28 09:11:23 +0200

    Switched away from staging

--- nginx.nix
+++ nginx.nix
@@ -53,7 +53,6 @@
         CF_DNS_API_TOKEN_FILE=/run/secrets/cloudflare-dns-api-token
       ''}";
       webroot = null;
-      server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     };
   };
   users.users.nginx.extraGroups = [ "acme" ];
