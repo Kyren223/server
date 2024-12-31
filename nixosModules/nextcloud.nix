a100cbf68dc3b21cc0926b065e225396292ac08b
Kyren223 <Kyren223@proton.me>
2024-12-31 22:33:01 +0200

    Added user account declaratively

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -2,6 +2,9 @@
 
   imports = [
     ./acme.nix
+    "${fetchTarball {
+    url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+    sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
   ];
 
   options = {
@@ -25,6 +28,8 @@
     sops.secrets.nextcloud-admin-password = { owner = "nextcloud"; group = "nextcloud"; };
     services.nextcloud.phpOptions."realpath_cache_size" = "0";
 
+    sops.secrets.nextcloud-kyren-password = { owner = "nextcloud"; group = "nextcloud"; };
+
     services.nextcloud = {
       enable = true;
       package = pkgs.nextcloud29;
@@ -33,6 +38,12 @@
       config = {
         dbtype = "pgsql";
         adminpassFile = config.sops.secrets.nextcloud-admin-password.path;
+      };
+      ensureUsers = {
+        kyren = {
+          email = "kyren223@proton.em";
+          passwordFile = config.sops.secrets.nextcloud-kyren-password.path;
+        };
       };
     };
   };
