896f2fce41a70aa02a931b3c62f3d98801ccc132
Kyren223 <Kyren223@proton.me>
2024-12-31 16:43:07 +0200

    Added syncthing user

--- nixosModules/syncthing.nix
+++ nixosModules/syncthing.nix
@@ -25,6 +25,13 @@
       locations."/".proxyPass = "http://localhost:8384/";
     };
 
+    users.users.syncthing = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO7P9K9D5RkBk+JCRRS6AtHuTAc6cRpXfRfRMg/Kyren"
+      ];
+    }
+
     services.syncthing = {
       enable = true;
       group = "syncthing";
