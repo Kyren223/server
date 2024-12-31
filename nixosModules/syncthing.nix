40f67cc5d32520de4bb8d019a933947c97726e7c
Kyren223 <Kyren223@proton.me>
2024-12-31 16:30:31 +0200

    Updated syncthing to be web-accessible

--- nixosModules/syncthing.nix
+++ nixosModules/syncthing.nix
@@ -1,15 +1,29 @@
 { pkgs, lib, config, ... }: {
 
+  imports = [
+    ./acme.nix
+  ];
+
   options = {
     syncthing.enable = lib.mkEnableOption "enables syncthing";
   };
 
   config = lib.mkIf config.syncthing.enable {
-
+    # 443 and 80 for for http and https
     # 22000 TCP and/or UDP for sync traffic
     # 21027/UDP for discovery
-    networking.firewall.allowedTCPPorts = [ 22000 ];
+    networking.firewall.allowedTCPPorts = [ 22000 443 80 ];
     networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
+    # Make sure acme module is active for the "kyren.codes" ssl cert
+    acme.enable = true;
+
+    services.nginx.enable = true;
+    services.nginx.virtualHosts."sync.kyren.codes" = {
+      useACMEHost = "kyren.codes";
+      forceSSL = true;
+      locations."/".proxyPass = "http://localhost:8384/";
+    };
 
     servicessyncthing = {
       enable = true;
@@ -25,6 +39,12 @@
         };
         folders = { };
       };
+    };
+
+    sops.secrets.syncthing-gui-password = { };
+    services.syncthing.settings.gui = {
+        user = "username";
+        password = ${config.sops.secrets.syncthing-gui-password.path};
     };
   };
 }
