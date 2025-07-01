afeeffea30028c6b0218d377532aae13a264af16
Kyren223 <Kyren223@proton.me>
2025-07-01 23:00:13 +0300

    Changed to config.alloy without eko- for debugging

--- nixosModules/eko.nix
+++ nixosModules/eko.nix
@@ -69,7 +69,7 @@
     loki.enable = true;
     services.alloy.enable = true;
     environment.etc = {
-      "alloy/eko-config.alloy".text = builtins.readFile ./eko-config.alloy;
+      "alloy/config.alloy".text = builtins.readFile ./eko-config.alloy;
     };
 
   };
