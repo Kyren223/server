abeeeb2a45d8f450904068d2cfaa408ba15b9e01
Kyren223 <kyren223@proton.me>
2025-08-03 20:24:59 +0300

    a

--- nixosModules/eko.nix
+++ nixosModules/eko.nix
@@ -28,6 +28,7 @@
     # Allow grafana access to the sqlite db
     users.users.eko.extraGroups = [ "grafana" ];
     # systemd.services.eko.serviceConfig.StateDirectoryMode = lib.mkForce 0750;
+    systemd.services.eko.serviceConfig.StateDirectory = "eko";
     systemd.services.grafana = {
       serviceConfig = {
         ProtectHome = lib.mkForce false;
