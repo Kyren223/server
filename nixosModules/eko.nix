2e0e24b411b7d0e74987cf8156e335d02c2d5c27
Kyren223 <kyren223@proton.me>
2025-08-03 20:04:40 +0300

    Maybe fixed systemd issue?

--- nixosModules/eko.nix
+++ nixosModules/eko.nix
@@ -26,8 +26,7 @@
     ];
 
     # Allow grafana access to the sqlite db
-    systemd.tmpfiles.rules = [ "d /var/lib/eko 0755 eko eko" ];
-    systemd.services.eko.serviceConfig.StateDirectoryMode = lib.mkForce "0755";
+    systemd.services.eko.serviceConfig.StateDirectoryMode = lib.mkForce 0755;
     systemd.services.grafana = {
       serviceConfig = {
         ProtectHome = lib.mkForce false;
