24136596fde9ebd03d21b3920f3e76a40a99ebeb
Kyren223 <kyren223@proton.me>
2025-08-08 19:17:46 +0300

    Updated eko to version 0.1.0 release

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix/bd695cc4d0a5e1bead703cc1bec5fa3094820a81";
-    eko.url = "github:kyren223/eko";
+    eko.url = "github:kyren223/eko/v0.1.0";
   };
 
   outputs = { nixpkgs, disko, sops-nix, eko, ... }: {
