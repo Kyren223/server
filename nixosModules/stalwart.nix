45d99316df231546f9f8d84073fdfffccddf9f7b
Kyren223 <contact@kyren.codes>
2025-08-24 13:47:19 +0300

    Added nextcloud email

--- nixosModules/stalwart.nix
+++ nixosModules/stalwart.nix
@@ -21,6 +21,7 @@
     sops.secrets.stalwart-kyren-password = { owner = "stalwart-mail"; group = "stalwart-mail"; };
     sops.secrets.stalwart-eko-password = { owner = "stalwart-mail"; group = "stalwart-mail"; };
     sops.secrets.stalwart-git-password = { owner = "stalwart-mail"; group = "git"; mode = "0440"; };
+    sops.secrets.stalwart-nextcloud-password = { owner = "stalwart-mail"; group = "stalwart-mail"; };
     sops.secrets.stalwart-postmaster-password = { owner = "stalwart-mail"; group = "stalwart-mail"; };
 
     services.nginx.virtualHosts."webadmin.kyren.codes" = {
@@ -140,6 +141,12 @@
               name = "git";
               secret = "%{file:${config.sops.secrets.stalwart-git-password.path}}%";
               email = [ "git@kyren.codes" ];
+            }
+            {
+              class = "individual";
+              name = "nextcloud";
+              secret = "%{file:${config.sops.secrets.stalwart-nextcloud-password.path}}%";
+              email = [ "nextcloud@kyren.codes" ];
             }
             {
               class = "individual";
