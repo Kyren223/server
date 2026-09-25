e0ec8f7cc5a4a0e1eff37ff2c5680b558d1b48fd
Kyren223 <contact@kyren.codes>
2026-09-25 12:00:07 +0300

    Update to nextcloud 33

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -31,7 +31,7 @@
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud32;
+      package = pkgs.nextcloud33;
       hostName = "nextcloud.kyren.codes";
 
       database.createLocally = true;
